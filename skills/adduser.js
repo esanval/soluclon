c2be4a7d59ae52751ca7872de7b8d8922268be84
Gogs <gogs@fake.local>
2018-03-01 16:29:07 +0100

    err

--- skills/adduser.js
+++ skills/adduser.js
@@ -1,14 +1,13 @@
-module.exports = function(controller) {
-  controller.hears(["add","agregar"], 'direct_message,direct_mention', function(bot, message) {
-    var email = message.user;
-    var CiscoSpark = require('node-ciscospark');
-    var async = require('async');
-    var spark = new CiscoSpark(process.env.SPARK_TOKEN);
-    console.log("Paso por aquí???");
+controller.hears(["add", "agregar"], 'direct_message,direct_mention', function(bot, message) {
+var email = message.user;
+var CiscoSpark = require('node-ciscospark');
+var async = require('async');
+var spark = new CiscoSpark(process.env.SPARK_TOKEN);
+console.log("Paso por aquí???");
 
-    bot.startConversation(message, function(err, convo) {
-      var question = "A quién deseas añadir a la sala?";
-      convo.ask(question, [{
+bot.startConversation(message, function(err, convo) {
+  var question = "A quién deseas añadir a la sala?";
+  convo.ask(question, [{
         default: true,
         callback: function(response, convo) {
           var membershipParameters = {
@@ -19,20 +18,18 @@ module.exports = function(controller) {
           var newUser = response.text;
           convo.say("Añadiendo algo espacio el usuario" + "**" + newUser + "**...");
           async.waterfall([
-            function(callback) {
-              spark.memberships.create(membershipParameters, function(err, response) {
-                if (err) {
-                  console.log(err);
-                  convo.say("No hemos podido crear el usuario: " + newUser);
-                  convo.next();
-                }
+              function(callback) {
+                spark.memberships.create(membershipParameters, function(err, response) {
+                    if (err) {
+                      console.log(err);
+                      convo.say("No hemos podido crear el usuario: " + newUser);
+                      convo.next();
+                    }
+                });
               }
-            }
-          ]);
-            };
           ]);
         }
-      }]);
-    });
+      }]
+    );
   });
-}
+});
