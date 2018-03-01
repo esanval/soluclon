0bebb159d21aa8d26bc0a93f74b55f5788b75ffc
Gogs <gogs@fake.local>
2018-03-01 16:40:04 +0100

    buff

--- skills/adduser.js
+++ skills/adduser.js
@@ -1,11 +1,36 @@
-controller.hears(["add", "agregar"], 'direct_message,direct_mention', function(bot, message) {
-var email = message.user;
-var CiscoSpark = require('node-ciscospark');
-var async = require('async');
-var spark = new CiscoSpark(process.env.SPARK_TOKEN);
-console.log("Paso por aquí???");
+module.exports = function(controller) {
+  controller.hears(["add", "agregar"], 'direct_message,direct_mention', function(bot, message) {
+    var email = message.user;
+    var CiscoSpark = require('node-ciscospark');
+    var async = require('async');
+    var spark = new CiscoSpark(process.env.SPARK_TOKEN);
+    console.log("Paso por aquí???");
 
-bot.startConversation(message, function(err, convo) {
-  var question = "A quién deseas añadir a la sala?";
+    bot.startConversation(message, function(err, convo) {
+      var question = "A quién deseas añadir a la sala?";
+      convo.ask(question, [{
+        default: true,
+        callback: function(response, convo) {
+          var membershipParameters = {
+            "roomId": message.channel,
+            "personEmail": response.text,
+            "isModerator": true
+          };
+          var newUser = response.text;
+          convo.say("Añadiendo algo espacio el usuario" + "**" + newUser + "**...");
+          async.waterfall([
+            function(callback) {
+              spark.memberships.create(membershipParameters, function(err, response) {
+                if (err) {
+                  console.log(err);
+                  convo.say("No hemos podido crear el usuario: " + newUser);
+                  convo.next();
+                }
+              });
+            }
+          ]);
+        }
+      }]);
+    });
   });
-});
+}
