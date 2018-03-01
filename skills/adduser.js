0ca766f27410011ee1dfe16ee4671d1b27a69933
Gogs <gogs@fake.local>
2018-03-01 16:17:11 +0100

    error

--- skills/adduser.js
+++ skills/adduser.js
@@ -1,9 +1,10 @@
 module.exports = function(controller) {
-  controller.hears(["add"], 'direct_message,direct_mention', function(bot, message) {
+  controller.hears(["add","agregar"], 'direct_message,direct_mention', function(bot, message) {
     var email = message.user;
     var CiscoSpark = require('node-ciscospark');
     var async = require('async');
     var spark = new CiscoSpark(process.env.SPARK_TOKEN);
+    console.log("Paso por aquí???");
 
     bot.startConversation(message, function(err, convo) {
       var question = "A quién deseas añadir a la sala?";
@@ -27,6 +28,8 @@ module.exports = function(controller) {
                 }
               }
             }
+          ]);
+            };
           ]);
         }
       }]);
